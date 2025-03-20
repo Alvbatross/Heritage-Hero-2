684cf8e2df70712119368e3cb9920873c424f48c
kennethkingko <kennethkingko@gmail.com>
2025-03-20 20:51:49 +0800

    Some improvements to the home mesh. Also placed teleporters and other sfx.
    AVRE component improvements too, will carry over to AVRE branch.

--- addons/ateneo-virtual-reality-escape/event-management-system/event.gd
+++ addons/ateneo-virtual-reality-escape/event-management-system/event.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:04257b371e8d9b2cf3e6e256f5cc6ebf40c3d68c46fd17ec74fb0773dee54359
-size 206
+oid sha256:4ee7a75778eecb17b24e012ba0e7151d9be0820596b942e90d85bae6a3b7b13d
+size 34
