025fcf876bfb37dc26d7a80c95d6f377eedd07e1
Alvbatross <alvaropato54@gmail.com>
2025-04-07 13:48:42 +0800

    Minor edit to quest_control

--- src/quest_control.gd
+++ src/quest_control.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f485169db1d6014482a655d2d8c7647d656fe545edea980de1381dc20f9add8
-size 823
+oid sha256:91290e944c73ac095330d67b8f56cf0963fefe5fb0f8efa984df60521a247ac3
+size 824
