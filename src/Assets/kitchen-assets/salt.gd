d3e4f9b1a04d0f37646c8574c87f568f15f45403
kennethkingko <kennethkingko@gmail.com>
2025-04-08 20:09:23 +0800

    Added improvements to kitchen items, still WIP. (Will work on bowl parts tomorrow).

---
+++ src/Assets/kitchen-assets/salt.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5ee064bb255dde80e2359ca868e0d501b026ec885f119b3bec38ec7a54325ed4
+size 461
---
+++ src/Assets/kitchen-assets/yeast.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:753925f91f29f4f78270d1922c64fdb66c66aa2ae075b7f280ad7f3e167951d7
+size 463
