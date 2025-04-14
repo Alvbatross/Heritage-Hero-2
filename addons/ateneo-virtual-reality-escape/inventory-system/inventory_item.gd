f12e2fa2ae19fc0eb82c5287f30667e34a23d6f3
kennethkingko <kennethkingko@gmail.com>
2025-04-14 18:35:14 +0800

    Oven functionality working (half), improved journal inventory behavior.
    - To work on adding tray with dough, kneading, and the multiple dough tscns

--- addons/ateneo-virtual-reality-escape/inventory-system/inventory_item.gd
+++ addons/ateneo-virtual-reality-escape/inventory-system/inventory_item.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31ae3a15d9f9f55bae985697da74b58bbec2865461dab2d6a71ff739fb7b5ceb
-size 6418
+oid sha256:d7e2e5dc0ad7dc2bc46bae773011190485399a2b26d0ef588e6737adda5cba63
+size 6447
---
+++ src/Assets/kitchen-assets/oven.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:97b7a6d75a7bbe20ae4b7fc4565f9554f18aa62794b28b3ed85f9a1e152faab0
+size 804
