6345f9b1cde8261582cff8bb19b0e3dbc10137f9
kennethkingko <kennethkingko@gmail.com>
2025-04-21 14:42:19 +0800

    Minor improvements on interactions with dough, oven and journal
    - Journal interactable handle issue fixed (can be grabbed while invisible before)
    - Added kneading on dough (do it while it's on bowl)
    - Now easier to interact with the oven door handles
    - Tray now should not affect items in its slots

--- src/Assets/00-general-3d-assets/tray_new.gd
+++ src/Assets/00-general-3d-assets/tray_new.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d117f21a3f587bd5b5d2e9fbb2b7607a82dfc8861aa357b542d9d1894d6fe5e9
-size 1375
+oid sha256:d6362e5cc8ac3e02cff31db88a89d19a6c7ff95dd73a2a15440710128672f3f1
+size 1393
---
+++ src/Assets/kitchen-assets/dough.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:70081c3e1dce098e1a616dce34262b03d674586b9ca2b493ee8fcada215b3f68
+size 676
