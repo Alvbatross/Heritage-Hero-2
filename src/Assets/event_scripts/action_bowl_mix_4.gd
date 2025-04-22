2398928b5b43dc181f5dbf0fdee81aeebca27e98
kennethkingko <kennethkingko@gmail.com>
2025-04-22 17:01:07 +0800

    Baking level 99% complete (need polish), jolt physics fix tested and confirmed working.
    Will continue work on the other parts later.

--- src/Assets/event_scripts/action_bowl_mix_4.gd
+++ src/Assets/event_scripts/action_bowl_mix_4.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7a409b0f89de7d05f683e47642f5c389321fa88bd6e06485f7189d2aa6c9272
-size 157
+oid sha256:a5d0968537ebb216a3e7fe7a24c11c50ab11f098ba624af21d3d31d5243e1cb6
+size 254
--- src/Assets/kitchen-assets/bowl_main.gd
+++ src/Assets/kitchen-assets/bowl_main.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:619935778bc7c2e343bb09289fecf2981772c9c6b0dc1987423353bc6577e688
-size 6106
+oid sha256:19f4aaae6aa9b3c5c70f838d88f49bccb4e01a3ac56e194bba33835be2a336cf
+size 6244
