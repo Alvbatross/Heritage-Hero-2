de17a46ef66ce487cd6e2f2667da057b4a43782e
Alvbatross <alvaropato54@gmail.com>
2025-04-25 16:39:34 +0800

    Made progress for part 3. Arranging the pictures part is almost done.

--- src/main.gd
+++ src/main.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e32c16f2c66d131bfa26337b5396a5ab07794f600d57cf8033c6186a2800a32
-size 1490
+oid sha256:537b19acd7f53e8de5fff7d1df67e793738e17e3e3854599864f77e2671d1d61
+size 1706
