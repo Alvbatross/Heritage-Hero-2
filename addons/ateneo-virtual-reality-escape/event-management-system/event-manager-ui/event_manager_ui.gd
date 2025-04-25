19cf6da948f61f6dc65087e2c350db344bdd0dad
Alvbatross <alvaropato54@gmail.com>
2025-04-25 17:46:18 +0800

    Minor fix to event editor UI for checkbox alignment

--- addons/ateneo-virtual-reality-escape/event-management-system/event-manager-ui/event_manager_ui.gd
+++ addons/ateneo-virtual-reality-escape/event-management-system/event-manager-ui/event_manager_ui.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d59767aec06ba2bb79001f7b18e47d0881dce828b095e445ecb5bbcdb5a61f02
+oid sha256:5d168cf165057370a511f8b70fa7adc9d8bf501fe2494bf79a37dcef716cc293
 size 49494
