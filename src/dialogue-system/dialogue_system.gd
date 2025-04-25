a1a2a023aa0f43be7be455396483d03ff47e38ad
kennethkingko <81554822+kennethkingko@users.noreply.github.com>
2025-04-25 16:26:33 +0800

    Dialogue system QoL - allow non-clickable dialogue to finish when clicking the button.
    - Also fixed weird tray behavior (collision shape became too huge for some reason..)

--- src/dialogue-system/dialogue_system.gd
+++ src/dialogue-system/dialogue_system.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6dca5d4df97db54e5114cee02cc3b0b176f92c86d9dbd9d85c5b1ccfb2c5467
-size 8618
+oid sha256:a970e823d34c1e99f25417dfa375bc392ef266e645841939e076ba709444965e
+size 8953
