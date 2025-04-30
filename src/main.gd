9a8029a64e2b3aeff6345f571889dc3a4dde5b82
Alvbatross <55185563+Alvbatross@users.noreply.github.com>
2025-04-30 15:17:03 +0800

    Change project.godot to limit max fps.

--- src/main.gd
+++ src/main.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2774c04a3c6df8b83dc7735b48f071a756384b88df5c6295e679d0f6f3744acd
-size 3246
+oid sha256:b1a44297e2f12d75aeb51980af90a01911556a6fc8ad5b424f1a1ee7b9c14613
+size 3655
