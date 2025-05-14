52af949bbc678e412f58cf1f999487a43c569464
Alvbatross <55185563+Alvbatross@users.noreply.github.com>
2025-05-14 10:28:27 +0800

    added flashlight hip slot to keep flashlight on even when not being held

--- src/ois-objects/tutorial/flashlight_control.gd
+++ src/ois-objects/tutorial/flashlight_control.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4160cdefe0023df34e0bc6cc32971e002865bda3ce5186f93210c832e55cc837
-size 377
+oid sha256:bc3b157be33ff7d271cca20da23deb1d1fe16db1f14eac150a91b0d65853709e
+size 356
