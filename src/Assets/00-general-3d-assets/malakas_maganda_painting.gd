049f96cd757c3a303588ef803ecd4b86cc227377
kennethkingko <kennethkingko@gmail.com>
2025-05-14 17:53:26 +0800

    Added intro sequence and mansion exterior.

--- src/Assets/00-general-3d-assets/malakas_maganda_painting.gd
+++ src/Assets/00-general-3d-assets/malakas_maganda_painting.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc895b4e3dbbd83343ed9ae7cb5e43c424e1e7a1b1568ac408aa85960ece2ca4
-size 1127
+oid sha256:e430ab857537120c68e0f35ed07d8b707e97af25fd7cc2244b5eda888725bc56
+size 1125
--- src/main.gd
+++ src/main.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95c3e346cf2dbc80bd8d079f2be72a52f10518eaecdf2d1bb9d6f0a9682c14ee
-size 3823
+oid sha256:425aa25d75e8b9e4505b508c46b87f8a145be80d8f9b7b0d2c016b5e23b6edf4
+size 4087
