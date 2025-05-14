4246d9d59856310db4ad02051a9b6782f2014bf8
Alvbatross <55185563+Alvbatross@users.noreply.github.com>
2025-05-14 17:11:46 +0800

    Preliminary Inventory fix applied. Will continue working on it for a smoother experience

--- addons/ateneo-virtual-reality-escape/inventory-system/inventory_system.gd
+++ addons/ateneo-virtual-reality-escape/inventory-system/inventory_system.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64c9302b038e56c5c773bba9063f66bcde328e87b9f6e5752e3629739e34d0be
-size 7842
+oid sha256:2ea88964a2a4f3f372ea348286df289dc589c3010b08d8a961c636cc0e40cf21
+size 8276
