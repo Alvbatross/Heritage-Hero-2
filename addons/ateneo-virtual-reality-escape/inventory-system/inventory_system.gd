aa1e4090b1d2b85bcd530945f517ef4e25bc91e7
Alvbatross <alvaropato54@gmail.com>
2025-05-15 08:50:14 +0800

    Reduced the amount of jumping during the rotation, but it still occurs occasionally.

--- addons/ateneo-virtual-reality-escape/inventory-system/inventory_system.gd
+++ addons/ateneo-virtual-reality-escape/inventory-system/inventory_system.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b6109d9c0239b9d448c936ee4a072ec1d9079dc43593276a686ef535e274c04
-size 8942
+oid sha256:a87e8f5073284014ff56efb24d5ab8251be87dd98beeedf860df8a3070f23adc
+size 9374
