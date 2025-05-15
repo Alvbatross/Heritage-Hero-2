fde582f359b1667c1704e0cc534e1f8f06d67e79
Alvbatross <55185563+Alvbatross@users.noreply.github.com>
2025-05-15 15:13:21 +0800

    WIP: Attaching pieces of the document to each other

---
+++ src/Assets/study-assets/malakas-at-maganda-doc/custom_attach_component.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a2074665ec88b14368d7dedb4b50bf1d763fcd85c3937490f4ce1b344dae79d1
+size 1260
---
+++ src/Assets/study-assets/malakas-at-maganda-doc/malakas_at_maganda_doc.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d23eeee46f1dd82d1d8be02800f526c93a46cad5fb049ba3577980aad9ba3fde
+size 2049
