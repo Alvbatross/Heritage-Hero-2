2e824ffe7a285f51a7c7e57fc74dee48426fb074
kennethkingko <kennethkingko@gmail.com>
2025-05-25 15:01:33 +0800

    Add Rolex Watch entry to journal

--- src/ois-objects/tutorial/key_items_container.gd
+++ src/ois-objects/tutorial/key_items_container.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b4fffdb08cba36a7f868c94be585e44f3bd5e47d9bd1ef4a71683b137093c6b
-size 1996
+oid sha256:4242bc21fac9b59b8a5587b1f03a25279c3ee3706c870214e01d365fb8a30a8c
+size 2111
