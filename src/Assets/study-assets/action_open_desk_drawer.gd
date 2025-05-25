555bcdafb9130548b464ada02ca6a6241c97a069
Alvbatross <alvaropato54@gmail.com>
2025-05-25 16:36:16 +0800

    Added some Key items and Cassettes.

--- src/Assets/study-assets/action_open_desk_drawer.gd
+++ src/Assets/study-assets/action_open_desk_drawer.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:151c6dccc9f8e19eaf33cc2000833c06e34b4851af42f3789c95c7d976dbf68a
-size 614
+oid sha256:c30070ba84eadaa0a69f9c841b3aaae7404d3a78778296eee62fec9b27846e0a
+size 829
--- src/ois-objects/event-scripts/cassette_event.gd
+++ src/ois-objects/event-scripts/cassette_event.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a4529ac2c52229c6b477e14ae86ba62caa06f7d577c46a13746fe654e39cbc6
-size 967
+oid sha256:dbcbd0a3a3cbb3de2b516b711934e62d7cd4ce006b1c792f10188bd4a0e7f9a2
+size 966
