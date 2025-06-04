5faabc708253e453674451081640db66697a2865
Alvbatross <alvaropato54@gmail.com>
2025-06-04 14:01:44 +0800

    Teleporter Active re-added

--- addons/ateneo-virtual-reality-escape/teleportation-system/teleporter.gd
+++ addons/ateneo-virtual-reality-escape/teleportation-system/teleporter.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a49abd53b3b9908b0d8ce93f58bd394e3a166d536860f5ea1668dbd58ec2a4e
-size 4740
+oid sha256:f744b2c5c57a0749f365e73a0d948195d8dd0f10c2471d9aad73d0cdf14cb4dc
+size 4784
