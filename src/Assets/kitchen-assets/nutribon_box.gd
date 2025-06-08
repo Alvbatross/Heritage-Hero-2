0bf69c66947380a51484f0c86ca146ad4dd43a4e
kennethkingko <kennethkingko@gmail.com>
2025-06-08 14:34:45 +0800

    Updated teleporter spectator camera settings + others
    - Fixed nutribun box not working.
    - Possibly fixed flashlight colliding with other items.

--- src/Assets/kitchen-assets/nutribon_box.gd
+++ src/Assets/kitchen-assets/nutribon_box.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4fd620958cb26cfc3ec5242f410f533a12c23bf2f60fe4469a3493b0adb98071
-size 2322
+oid sha256:b231b0e2f9f327dc7b0f8e5161e3b246016af38cb49fac96a3e2042ea017229b
+size 2245
