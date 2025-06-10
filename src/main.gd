d0ff397539b632fb29bbbecf785cd6b073effd25
mich-ca <michelle.abarico@student.ateneo.edu>
2025-06-10 11:51:22 +0800

    addressed some of the feedback from internal testing. Part 1

--- src/main.gd
+++ src/main.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9cb0dbec7379ee620fc57412a99f3905b8f689ee5d3d5ab08b87f79644c89a2b
-size 4578
+oid sha256:574d778d0410fc68044615089c2663f5638cbe0a0c07da9f43e312069bccde52
+size 4772
