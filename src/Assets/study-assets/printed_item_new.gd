3bdc9e10afda1bf0865737ef564265e19d372008
kennethkingko <kennethkingko@gmail.com>
2025-06-11 00:22:59 +0800

    Initial work on printed item and updated newspaper clippings.

---
+++ src/Assets/study-assets/printed_item_new.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4c58df5324da373a038d4ba350ea61bfd25a933b4d6deb3b3543b7bfc0bdf7e7
+size 271
---
+++ src/Assets/study-assets/printed_items_control.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e2ab36febf1c0b43ef4fadc7e3b47e3960b9f9ea631e0950a83cd6fe53c992c6
+size 1564
